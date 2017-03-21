e70de45c5a1701566c1b4d5114b58f301a926a09
a <a>
2017-03-20 19:48:33 -0700

    starting on redesign

--- Input System Experimentation/cl_Input.h
+++ Input System Experimentation/cl_Input.h
@@ -1,85 +1,88 @@
 #pragma once
 #include <unordered_map>
-#include <string>
-#include <sstream>
 #include <array>
+#include <chrono>
 #include "cl_Window.h"
 
-// RIN https://msdn.microsoft.com/en-us/library/windows/desktop/ms645543(v=vs.85).aspx
-// XIN https://msdn.microsoft.com/en-us/library/windows/desktop/ee417001(v=vs.85).aspx
+//RIN https://msdn.microsoft.com/en-us/library/windows/desktop/ms645543(v=vs.85).aspx
+//XIN https://msdn.microsoft.com/en-us/library/windows/desktop/ee417001(v=vs.85).aspx
+//~~_ repeat
+//~~_ dead zones
 
 class Input {
 public:
   Input(Window& win);
+  void update();
 
-  enum DeviceType: uint16_t {
-    INVALID,
-    MOUSE,
-    KEYBOARD,
-    XIN
+  struct MouseState {
+    struct Button {
+      bool triggered, held, released;
+    };
+
+    static const size_t BUTTON_CT = 5;
+    Button buttons[BUTTON_CT];
+    int dX, dY, dWheel;
   };
 
-  struct KeyBindRecord {
-    KeyBindRecord(DeviceType dev, short keyCode) : value(keyCode + (static_cast<uint32_t>(dev) << 16)) {}
-    const uint32_t value;
+  const MouseState& mouse() const { return mouseState; }
 
-    DeviceType  type() const { return static_cast<DeviceType>(value >> 16); }
-    uint16_t keyCode() const { return static_cast<unsigned short>(value && 16); }
+  static const unsigned int REPEAT_DELAY_MS = 500;
+  static const unsigned int REPEAT_FREQ_MS = 100;
 
-    operator uint32_t() const { return value; }
+  struct KeyboardState {
+    struct Key {
+      bool triggered, held, released, repeating;
+
+      void trigger() {
+        triggered = true;
+        held = true;
+        triggerTimeMS = millisecondsSinceEpoch();
+      }
+
+      void release() {
+        released = true;
+        held = false;
+      }
+
+      unsigned int millisecondsSinceEpoch() const {
+        auto now = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+      }
+
+      void calcRepeat(unsigned int msSinceEpoch) {
+        //true on trigger frame
+        repeating = triggered;
+        if(repeating) { return; }
+
+        //false prior to delay elapsed
+        int elapsedMS = msSinceEpoch - triggerTimeMS;
+        int postDelayMS = elapsedMS - REPEAT_DELAY_MS;
+        if(postDelayMS < 0) { return; }
+
+        //number of repeats that should have happened by now
+        unsigned int repeatTarget = postDelayMS / REPEAT_FREQ_MS;
+        //if it's increased since the last poll then repeat
+        repeating = repeatPrev < repeatTarget;
+
+        repeatPrev = repeatTarget;
+      }
+
+    private:
+      unsigned int triggerTimeMS;
+      unsigned int repeatPrev;
+
+    };
+
+    static const size_t KEY_CT = 200;
+    Key keys[KEY_CT];
   };
 
-  void addKey(const std::string& name) { keys[name]; }
-  void bindKey(const std::string& name, KeyBindRecord key) { keyBinds[key] = name; }
-
-  void removeKey(const std::string& name);
-  void unbindKey(KeyBindRecord key);
-  void resetKeyBinds();
-
-  void addAxis();
-  void removeAxis();
-  void bindAxis();
-  void unbindAxis();
-  void resetAxisbinds();
-
-  bool trigger(const std::string& key) const;
-  bool press(const std::string& key) const { return keys.at(key).press; }
-  bool release(const std::string& key) const;
-  int axis(const std::string& axis) const;
-
-  //~~_ repeat
-  //~~_ dead zones
+  const KeyboardState& keyboard() const { return kbState; }
 
 private:
-  enum Axis {
-    MOUSE_X,
-    MOUSE_Y,
-    MOUSE_WHEEL,
-    PAD_LX,
-    PAD_LY,
-    PAD_RX,
-    PAD_RY,
-    NUM_AXES
-  };
-
-  std::array<int, NUM_AXES> axes;
-
-  struct VKey {
-    unsigned int keyDownTimeStamp;
-    bool trigger, press, release;
-  };
-
-  static const int numMouseButtons = 5; //~~! detect
-
-  std::unordered_map<std::string, VKey> keys;
-  std::unordered_map<uint32_t, std::string> keyBinds;
-  //std::unordered_map<std::string, int> axes;
-  std::unordered_map<Axis, std::string> axisBinds;
+  MouseState mouseState;
+  KeyboardState kbState;
 
   LRESULT procFn(HWND hwnd, WPARAM wparam, LPARAM lparam);
-  void updateMouse(const RAWMOUSE& data);
-  void updateKeyboard(const RAWKEYBOARD& data) {} //~~_
-  void updateXInput() {} //~~_
 
 };
-
